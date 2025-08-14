f76e4f77c49b8ec9e57bf49c37ef4576add04fa9
Chim-SO <chimso1994@gmail.com>
2025-08-14 14:42:25 +0200

    nvok mlflow local or hwds hugs client

--- src/app/api/invocations/route.ts
+++ src/app/api/invocations/route.ts
@@ -13,25 +13,61 @@ interface ModelPrediction {
 export async function POST(request: Request) {
   try {
     const body = (await request.json()) as PredictRequest;
-
-    const client = await Client.connect("ChimSO/HWDC");
-    const result = await client.predict("/predict", {
-      image_base64: body.image_base64,
-    });
-
-    const predictions = result.data as ModelPrediction[];
-    if (!predictions) {
-      return NextResponse.json(
-        { error: "No prediction returned from model" },
-        { status: 500 }
-      );
-    }
-
-    return NextResponse.json({ prediction: predictions[0] });
+    if (process.env.PREDICT_API_TYPE === "hwdcclient")
+      return await predictWithHWDCHugs(body.image_base64);
+    else if (process.env.PREDICT_API_TYPE === "mlflowlocal")
+      return await predictWithMLflowLocalServer(body.image_base64);
   } catch (error: unknown) {
-    // Type guard: check if error is an Error object and has message
     const message = error instanceof Error ? error.message : "Unknown error";
 
     return NextResponse.json({ error: message }, { status: 500 });
   }
+}
+
+async function predictWithMLflowLocalServer(image_base64: string) {
+  const response = await fetch("http://127.0.0.1:5000/invocations", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      instances: [
+        {
+          image_base64: image_base64,
+        },
+      ],
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`ML server returned ${response.status}`);
+  }
+
+  const result = await response.json();
+  const prediction = (result.predictions ?? null) as ModelPrediction | null;
+  if (!prediction) {
+    return NextResponse.json(
+      { error: "No prediction returned from model" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ prediction });
+}
+
+async function predictWithHWDCHugs(image_base64: string) {
+  const client = await Client.connect("ChimSO/HWDC");
+  const result = await client.predict("/predict", {
+    image_base64: image_base64,
+  });
+
+  const predictions = result.data as ModelPrediction[];
+  if (!predictions) {
+    return NextResponse.json(
+      { error: "No prediction returned from model" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ prediction: predictions[0] });
 }
