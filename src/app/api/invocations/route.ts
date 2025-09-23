fcbc4e9ea20014070e8bc1462103fe77c7542420
Chim-SO <chimso1994@gmail.com>
2025-09-23 14:29:31 +0200

    MLflow url env

--- src/app/api/invocations/route.ts
+++ src/app/api/invocations/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request) {
 }
 
 async function predictWithMLflowLocalServer(image_base64: string) {
-  const response = await fetch("http://127.0.0.1:5000/invocations", {
+  const response = await fetch(process.env.MLFLOW_URL!, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
